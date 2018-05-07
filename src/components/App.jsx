c2e25a7ababdb1aec122bc981bcd13238dbb5526
Ainur Dzhaianbaeva and Qianqian Hu <ainur@>
2018-05-07 14:04:30 -0700

    add ticket controller page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
-import { Button } from 'react-bootstrap';
+import NewTicketControl from './NewTicketControl';
+// import { Button } from 'react-bootstrap';
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
+// import NewTicketForm from './NewTicketForm';
 import Error404 from './Error404';
 
 function App(){
@@ -12,7 +13,7 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
         <Route component={Error404} />
       </Switch>
     </div>
--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -3,22 +3,22 @@
 // function NewTicketForm(){
 //   return (
 //     <Button>test</Button>
-//     // <div>
-//     //   <form>
-//     //     <input
-//     //       type='text'
-//     //       id='names'
-//     //       placeholder='Pair Names'/>
-//     //     <input
-//     //       type='text'
-//     //       id='location'
-//     //       placeholder='Location'/>
-//     //     <textarea
-//     //       id='issue'
-//     //       placeholder='Describe your issue.'/>
-//     //     <button type='submit'>Help!</button>
-//     //   </form>
-//     // </div>
+//     <div>
+//       <form>
+//         <input
+//           type='text'
+//           id='names'
+//           placeholder='Pair Names'/>
+//         <input
+//           type='text'
+//           id='location'
+//           placeholder='Location'/>
+//         <textarea
+//           id='issue'
+//           placeholder='Describe your issue.'/>
+//         <button type='submit'>Help!</button>
+//       </form>
+//     </div>
 //   );
 // }
 //
